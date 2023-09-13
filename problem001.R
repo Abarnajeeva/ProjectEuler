eb17306734877a0c6d25f27e42b254f739f2cd30
Abarnajeeva <144874884+Abarnajeeva@users.noreply.github.com>
2023-09-13 10:26:32 +0530

    Update problem001.R
    c

--- problem001.R
+++ problem001.R
@@ -1,22 +1,25 @@
-## Project Euler 1: Multiples of 3 and 5
+#include <stdio.h>
 
-## Solution 1
-answer <- 0
-for (i in 1:999) {
-    if (i %% 3 == 0 | i %% 5 == 0) 
-        answer <- answer + i
+int sum_of_multiples(int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+    return sum;
 }
-print(answer)
 
-## Solution 2
-sum((1:999)[((1:999) %% 3 == 0) | ((1:999) %% 5 == 0)])
+int main() {
+    int T;
+    scanf("%d", &T);
 
-## Solution 3
-sum(unique(c(seq(3, 999, 3), seq(5, 999, 5))))
+    for (int t = 0; t < T; t++) {
+        int N;
+        scanf("%d", &N);
+        int result = sum_of_multiples(N);
+        printf("%d\n", result);
+    }
 
-## Solution 4
-SumDivBy <- function(m, n) {
-    p <- floor(n / m) * m # Round to multiple of n
-    return (p * (1 + (p / m)) / 2)
+    return 0;
 }
-SumDivBy(3, 999) + SumDivBy(5, 999) - SumDivBy(15, 999)
